9b4be87aa9cbb3b05be17750be44a0bedf4113b4
indonumberone <01muqsith@gmail.com>
2025-02-19 15:15:11 +0700

    dht22

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from umqtt.robust import MQTTClient
-
+from time import sleep, sleep_ms
 import machine as m
+import dht
 
-ubidotsToken = "BBUS-I93MQuistalSIcgLgFQGJ1fq19C79jVeha1BD2RYWjaxAGThqinIwqJ"
+ubidotsToken = "BBUS-09DidGsAEKGqkJ7U76nm3btnWFjgHP"
+
+# gps_serial = m.UART(2, baudrate=9600, tx=35, rx=32)
 
 clientID = "esp32wroom32"
-
+sensor = dht.DHT22(m.Pin(15))
 client = MQTTClient("clientID", "industrial.api.ubidots.com", 1883, user = ubidotsToken, password = ubidotsToken)
 
 def checkwifi():
 
     while not sta_if.isconnected():
 
-        time.sleep_ms(500)
+        sleep_ms(500)
 
         print(".")
 
@@ -27,19 +30,25 @@ def publish():
         checkwifi()
 
         client.connect()
-
-        lat = -7.280389
-
-        lng = 112.7867191
-
-        var = repr(pin13.value())
-
-        msg = b'{"location": {"value":%s, "context":{"lat":%s, "lng":%s}}}' % (var, lat, lng)
-
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        temp_f = temp * (9/5) + 32.0
+        print('Temperature: %3.1f C' %temp)
+        print('Temperature: %3.1f F' %temp_f)
+        print('Humidity: %3.1f %%' %hum)
+        # if gps_serial.any():
+        #    line = gps_serial.readline()  # Read a complete line from the UART
+        # if line:
+        #     line = line.decode('utf-8')
+        #     print(line.strip())
+        # sleep(0.5)
+        # msg = b'{"location": {"value":%s, "context":{"lat":%s, "lng":%s}}}' % (var, lat, lng)
+        msg = b'{"temperature": {"value":%s}, "humidity": {"value":%s}}' % (temp, hum)
         print(msg)
 
         client.publish(b"/v1.6/devices/ESP32_THSS", msg)
 
-        time.sleep(20)
+        sleep(20)
 
 publish()
