870116944df616ea1586df8ff1658346073f3ca4
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2025-02-23 11:36:07 +0700

    staging: implement gps pub

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from umqtt.robust import MQTTClient
 from time import sleep, sleep_ms
 from hcsr04 import HCSR04
+from lib.micropyGPS import MicropyGPS
 import machine as m
 import dht
 
 
 ubidotsToken = "BBUS-09DidGsAEKGqkJ7U76nm3btnWFjgHP"
 
-# gps_serial = m.UART(2, baudrate=9600, tx=35, rx=32)
+my_gps = MicropyGPS()
+gps_serial = m.UART(2, baudrate=9600, tx=17, rx=16)
+
+def convert_to_decimal(degrees, direction):
+    decimal = degrees[0] + (degrees[1] / 60)
+    if direction in ('S', 'W'):
+        decimal = -decimal
+    return decimal
 
 def led_cb(topic, msg):
     # print(msg)
@@ -50,6 +58,33 @@ pin13 = m.Pin(13, m.Pin.IN, m.Pin.PULL_UP)
 def publish():
     
     while True:
+        try:
+            while gps_serial.any():
+                data = gps_serial.read()
+                
+                for byte in data:
+                    status = my_gps.update(chr(byte))
+                    if status is not None:
+                        lat = convert_to_decimal(my_gps.latitude, my_gps.latitude[2])
+                        lon = convert_to_decimal(my_gps.longitude, my_gps.longitude[2])
+                        var = repr(pin13.value())
+                        
+                        msg = b'{"location": {"value":%s, "context":{"lat":%s, "lng":%s}}}' % (var, lat, lon)
+                        
+                        client.publish(b"/v1.6/devices/ESP32_THSS", msg)
+                    
+                        print('UTC Timestamp:', my_gps.timestamp)
+                        print('Date:', my_gps.date_string('long'))
+                        print(f"Latitude: {lat:.6f}, Longitude: {lon:.6f}")
+                        print('Altitude:', my_gps.altitude)
+                        print('Satellites in use:', my_gps.satellites_in_use)
+                        print('Horizontal Dilution of Precision:', my_gps.hdop)
+                        print()
+                
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            
+
         print("woalah gtu")
         checkwifi()
 
